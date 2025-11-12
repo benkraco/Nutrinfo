c8554a84814cca5110add2e5e1f394c353a48b23
ANDRES FURLAND OTERO <49437898@ort>
2025-11-12 14:23:39 -0300

    estoyCansadoJefe

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -3,8 +3,8 @@ namespace Nutrinfo.Models;
 public class Productos {
     public int Id { get; set; }
     public string Nombre { get; set; }
-    public string Marca { get; set; }
     public string ImagenProducto { get; set; }
     public string ImagenInformacion { get; set; }
     public int Cantidad { get; set; }
+    public string Tipo { get; set; }
 }
